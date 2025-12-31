2365f7dda3d0f5f59f5993c6dd93fadc3102ec34
Abdulla Al Shahariar <abdullaalshahariar@gmail.com>
2025-12-31 23:57:50 +0600

    now can switch between block or media option in side bar

--- frontend/builder/src/components/PatternContent.jsx
+++ frontend/builder/src/components/PatternContent.jsx
@@ -1,22 +1,9 @@
 import styles from "./PatternContent.module.css";
-import cross_icon from '../assets/icons/cross_icon.svg';
 import search_icon from '../assets/icons/search_icon.svg';
 
 export function PatternContent() {
     return (
         <div class={styles.pattern_content_container}>
-            <div class={styles.top_section}>
-                <button>
-                    <span>Block</span>
-                </button>
-                <button>
-                    <span>Pattern</span>
-                </button>
-                <button class={styles.close_button}>
-                    <img src={cross_icon} alt="close" />
-                </button>
-            </div>
-
             <div class={styles.search_bar}>
                 <img src={search_icon} alt="search" />
                 <input type="text" placeholder="Search" />
