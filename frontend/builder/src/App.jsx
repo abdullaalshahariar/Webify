f49580a0ea3c0ef219516eafc231e08b99757cdc
Abdulla Al Shahariar <abdullaalshahariar@gmail.com>
2025-12-31 17:50:03 +0600

    added sidebar. not complete yet

--- frontend/builder/src/App.jsx
+++ frontend/builder/src/App.jsx
@@ -1,9 +1,11 @@
+import { Sidebar } from "./components/Sidebar";
 import { Topbar } from "./components/Topbar";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <Sidebar />
     </div>
 
   );
---
+++ frontend/builder/src/components/Sidebar.jsx
@@ -0,0 +1,54 @@
+import styles from './Sidebar.module.css';
+import cross_icon from '../assets/icons/cross_icon.svg';
+import paragraph_icon from '../assets/icons/paragraph_icon.svg';
+import heading_icon from '../assets/icons/heading_icon.svg';
+import list_icon from '../assets/icons/list_icon.svg';
+import table_icon from '../assets/icons/table_icon.svg';
+
+export function Sidebar() {
+    return (
+        <div class={styles.sidebar_container}>
+            <div class={styles.top_section}>
+                <button>
+                    <span>Block</span>
+                </button>
+                <button>
+                    <span>Pattern</span>
+                </button>
+                <button class={styles.close_button}>
+                    <img src={cross_icon} alt="close" />
+                </button>
+            </div>
+
+            <span class={styles.label_span}>TEXT</span>
+
+            <div class={styles.text_items}>
+                <button>
+                    <img src={paragraph_icon} alt="paragraph" />
+                    <span>Paragraph</span>
+                </button>
+
+                <button>
+                    <img src={heading_icon} alt="heading" />
+                    <span>Heading</span>
+                </button>
+
+                <button>
+                    <img src={list_icon} alt="list" />
+                    <span>List </span>
+                </button>
+
+                <button>
+                    <img src={table_icon} alt="table" />
+                    <span>Table</span>
+                </button>
+            </div>
+
+            <span class={styles.label_span}>MEDIA</span>
+
+            <div>
+
+            </div>
+        </div>
+    )
+}
