5d1063869bd14e4a696ad02f733f4a9e35d8e1d2
Abdulla Al Shahariar <abdullaalshahariar@gmail.com>
2026-01-14 02:52:59 +0600

    Made the editor somewhat usable

--- frontend/builder2/SaaticBuilder2/src/editor/config.js
+++ frontend/builder2/SaaticBuilder2/src/editor/config.js
@@ -8,13 +8,20 @@ import 'grapesjs/dist/css/grapes.min.css';
 export const initEditor = (container) => {
     const editor = grapesjs.init({
         container: container,
+        height: '100%',
+        width: '100%',
         fromElement: true, //allows to modify existing HTML
 
-        height: '100vh',
-        width: 'auto',
+        //prevent loading default panels
+        panels: {
+            defaults: [],
+        },
 
         blockManager: {
             appendTo: '#blocks-container',
+        },
+        layerManager: {
+            appendTo: '#layers-container'
         },
         styleManager: {
             appendTo: '#styles-container',
@@ -23,12 +30,28 @@ export const initEditor = (container) => {
             appendTo: '#traits-container',
         },
 
+        // aetting up devices for the toggles to work
+        deviceManager: {
+            devices: [
+                { name: 'Desktop', width: '' }, // empty means width = 100%
+                { name: 'Tablet', width: '768px', widthMedia: '992px' },
+                { name: 'Mobile', width: '320px', widthMedia: '480px' },
+            ]
+        },
+
+        // Define commands for the buttons in SolidJS
+        // editor.Commands.add('set-device-desktop', {
+        //     run: (editor) => editor.setDevice('Desktop')
+        // }),
+        // editor.Commands.add('set-device-tablet', {
+        //     run: (editor) => editor.setDevice('Tablet')
+        // }),
+        // editor.Commands.add('set-device-mobile', {
+        //     run: (editor) => editor.setDevice('Mobile')
+        // }),
+
         //diable local database for now
         storageManager: false,
-
-        panels: {
-            defaults: [],
-        },
 
         //load the preset webpage plugin
         plugins: [gjsBlocksBasic],
